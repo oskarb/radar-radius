62a01079ce65ec645c298e2a4221291a38a4d48f
Jakob A. Dam <jakob.a.dam@gmail.com>
2015-04-20 11:40:40 +0200

    Don't wait before sending packet.

--- RADAR/RADIUSClient.vb
+++ RADAR/RADIUSClient.vb
@@ -5,24 +5,29 @@ Imports System.Collections
 Imports System.IO
 Imports System.Net
 Imports System.Reflection
-
+Imports System.IO.Path
+Imports System.IO.Directory
 
 
 Public Class RADIUSClient
+
     Private Const UDP_TTL As Integer = 10
-    Private pSSecret As String = Nothing
+
+    Private pSharedSecret As String
+
     Private pUsername As String = Nothing
     Private pPassword As String = Nothing
     Private pServer As String = Nothing
     Private pRadiusPort As Integer = 1812
     Private pUDPTimeout As Integer = 5000
-    Private pDebug As Boolean = False
+    Private pDebug As Boolean = True
     Private pRA As Byte() = New Byte(15) {}
     Private pClientIdentifier As Integer
     Private pEndpoint
 
-    Public Sub New(server As String, port As Integer)
-        pServer = Server
+    Public Sub New(server As String, port As Integer, sharedSecret As String)
+        pSharedSecret = sharedSecret
+        pServer = server
         pRadiusPort = port
     End Sub
 
@@ -44,8 +49,8 @@ Public Class RADIUSClient
         End Set
     End Property
 
-    Public Function Authenticate(SharedSecret As String, Username As String, Password As String, pAttributes As RADIUSAttributes) As RADIUSPacket
-        DebugOutput("Authenticate: SharedSecret: " & SharedSecret & " Username: " & Username & " Password: " & Password)
+    Public Function Authenticate(Username As String, Password As String, pAttributes As RADIUSAttributes) As RADIUSPacket
+        DebugOutput("Authenticate: " & " Username: " & Username)
 
         Dim pRandonNumber As New Random()
 
@@ -54,40 +59,35 @@ Public Class RADIUSClient
         Dim pIdentifier As Byte = Convert.ToByte(pRandonNumber.[Next](0, 32000) Mod 256)
 
         Dim pUserAttribute As New RADIUSAttribute(RadiusAttributeType.UserName, Username)
-        Dim pPassAttribute As New RADIUSAttribute(RadiusAttributeType.UserPassword, Crypto.GeneratePAP_PW(Password, SharedSecret, pRA))
+        Dim pPassAttribute As New RADIUSAttribute(RadiusAttributeType.UserPassword, Crypto.GeneratePAP_PW(Password, pSharedSecret, pRA))
 
         pAttributes.Add(pUserAttribute)
         pAttributes.Add(pPassAttribute)
 
         Dim request = New RADIUSPacket(pCode, pIdentifier, pAttributes, pRA)
+        Dim udpClient As New Sockets.UdpClient()
+        udpClient.Client.SendTimeout = pUDPTimeout
+        udpClient.Client.ReceiveTimeout = pUDPTimeout
+
+        udpClient.Ttl = UDP_TTL
+        udpClient.Connect(pServer, pRadiusPort)
+        udpClient.Send(request.Bytes, request.Bytes.Length)
 
         For i As Integer = 0 To 10
+            Threading.Thread.Sleep(20)
+
             Try
-                Dim myRadiusClient As New Sockets.UdpClient()
-                myRadiusClient.Client.SendTimeout = pUDPTimeout
-                myRadiusClient.Client.ReceiveTimeout = pUDPTimeout
-
-                myRadiusClient.Ttl = UDP_TTL
-                myRadiusClient.Connect(pServer, pRadiusPort)
-
-                Threading.Thread.Sleep(200)
-
                 DebugOutput("Sending data to radius server" & Encoding.Default.GetString(request.Bytes))
 
-                myRadiusClient.Send(request.Bytes, request.Bytes.Length)
-
-                Threading.Thread.Sleep(200)
-
                 Dim RemoteIpEndPoint As New IPEndPoint(IPAddress.Any, 0)
-
-                Dim receiveBytes As [Byte]() = myRadiusClient.Receive(RemoteIpEndPoint)
+                Dim receiveBytes As [Byte]() = udpClient.Receive(RemoteIpEndPoint)
                 DebugOutput("Receive data to radius server" & Encoding.Default.GetString(receiveBytes))
-                myRadiusClient.Close()
-
                 Dim response = New RADIUSPacket(receiveBytes)
                 Return response
             Catch e As Exception
                 DebugOutput("Exception Error: " + e.ToString())
+            Finally
+                udpClient.Close()
             End Try
         Next
         Return Nothing
@@ -106,13 +106,11 @@ Public Class RADIUSClient
     Private Sub DebugOutput(Output As String)
         If pDebug Then
 
-            Dim path As String = (New System.Uri(Assembly.GetExecutingAssembly().CodeBase)).AbsolutePath
-
-            path = Replace(Replace(path.ToLower, "/bin/radiusclient.dll", "/log/"), "/", "\")
-            '  Console.WriteLine("[DEBUG]  " + Output)
-            Dim myDebugWriter As New StreamWriter(path & "Radius_Debug.txt", True)
-            myDebugWriter.WriteLine(Now.ToString & ": " & "[DEBUG] " + Output)
-            myDebugWriter.Close()
+            Dim assemblyPath As String = (New System.Uri(Assembly.GetExecutingAssembly().CodeBase)).AbsolutePath
+            Dim logPath As String = GetParent(GetDirectoryName(assemblyPath)).FullName & "\log\Radius_Debug.txt"
+            Dim log As New StreamWriter(logPath, True)
+            log.WriteLine(Now.ToString & ": " & "[DEBUG] " + Output)
+            log.Close()
         End If
 
     End Sub
